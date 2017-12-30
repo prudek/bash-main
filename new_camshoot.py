dc2e8ac2fd5d987b97e51b9a15059a24d3951886
prudek <prudek@gmail.com>
2017-12-30 11:39:44 +0100

    .

--- new_camshoot.py
+++ new_camshoot.py
@@ -16,33 +16,16 @@ def takePicture():
     now = datetime.datetime.now() #+ datetime.timedelta(hours=2)
     fileName = time.strftime('%Hh%Mm%Ss.jpg', now.timetuple())
 
-    #fileName = time.strftime('%Hh%Mm%Ss.jpg')
   
-    if not os.path.exists('/mnt/var/www/webcam/' + dirMonth):
-        print os.makedirs('/mnt/var/www/webcam/' + dirMonth)
+    #if not os.path.exists('/mnt/var/www/webcam/' + dirMonth):
+    #    print os.makedirs('/mnt/var/www/webcam/' + dirMonth)
 
-    if not os.path.exists('/mnt/var/www/webcam/' + dirMonth + '/' + dirDay):
-        print os.makedirs('/mnt/var/www/webcam/' + dirMonth + '/' + dirDay)
+    #if not os.path.exists('/mnt/var/www/webcam/' + dirMonth + '/' + dirDay):
+    #    print os.makedirs('/mnt/var/www/webcam/' + dirMonth + '/' + dirDay)
 
-    os.system('wget -O /mnt/var/www/webcam/' + dirMonth  + '/' + dirDay  + '/' + fileName + ' http://admin:slonykapturek137@192.168.0.9/Streaming/channels/1/picture')	
-    #global previous
-    #global current
-    #previous = current
-    #current = '/mnt/var/www/webcam/' + dirMonth  + '/' + dirDay  + '/' + fileName	
+    os.system('wget -O /var/log/www/last.jpg ' http://admin:slonykapturek137@192.168.0.9/Streaming/channels/1/picture')	
    
  
-    #if len(previous) > 0:
-    #  sourceimg = Image(previous) 
-    #  targetimg = Image(current)
-    #  diffimg = sourceimg - targetimg
-    #
-    #  matrix = diffimg.getNumpy()
-    #  flat = matrix.flatten()
-    #  numchange = np.count_nonzero(flat)
-    #  percentchange = 100 * float(numchange) / float(len(flat))
-    # 
-    #  with open('/mnt/var/www/webcam/' + dirMonth  + '/' + dirDay  + '/' + 'statystyka.txt', 'a') as myfile:
-    #    myfile.write(current + '   '  + str(percentchange) + '%  \n')
 
 schedule.every(6).minutes.do(takePicture)
 
