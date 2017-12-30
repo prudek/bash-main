26679d839d8eee329cb9ce7d376f4f417df91bfb
prudek <prudek@gmail.com>
2017-12-30 09:36:39 +0000

    .

--- cpu_temp.sh
+++ cpu_temp.sh
@@ -6,7 +6,9 @@
 # -------------------------------------------------------
  
  
-cpu=$(</sys/class/thermal/thermal_zone0/temp) echo "$(date) @ $(hostname)" 
+cpu=$(</sys/class/thermal/thermal_zone0/temp) 
+
+echo "$(date) @ $(hostname)" 
 echo "-------------------------------------------" 
 echo "GPU => $(/opt/vc/bin/vcgencmd measure_temp)" 
 echo "CPU => $((cpu/1000))'C"
